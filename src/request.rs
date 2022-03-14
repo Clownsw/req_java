c0c76342532128a497a7f6c5679352011d5b5ac9
Smliexx <msmliexx1@gmail.com>
2022-03-14 08:00:49 +0800

    增加代码健壮性

--- src/request.rs
+++ src/request.rs
@@ -40,13 +40,17 @@ pub extern "system" fn Java_cn_smilex_req_Requests__1request(
     class: JClass,
     http_request: JObject,
 ) -> jobject {
+    // 请求URL
     let url = util::get_jstring_to_string(&env, "url", &http_request);
 
+    // 请求方式
     let method = util::get_jint_to_i32(&env, "method", &http_request);
 
+    // 请求体
     let body = util::get_jstring_to_string(&env, "body", &http_request);
     let body_status = body.eq("");
 
+    // 请求头
     let headers = util::parse_hash_map(
         &env,
         &env.get_field(http_request, "headers", util::JAVA_CLASS_HASH_MAP)
@@ -55,6 +59,7 @@ pub extern "system" fn Java_cn_smilex_req_Requests__1request(
             .unwrap(),
     );
 
+    // 查询参数
     let params = util::parse_hash_map(
         &env,
         &env.get_field(http_request, "params", util::JAVA_CLASS_HASH_MAP)
@@ -63,8 +68,10 @@ pub extern "system" fn Java_cn_smilex_req_Requests__1request(
             .unwrap(),
     );
 
+    // 最大重定向
     let max_redirect: usize = util::get_jint_to_i32(&env, "maxRedirect", &http_request) as usize;
 
+    // 重定向URL列表
     let redirect_url_list: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
 
     let redirect_url_list_clone = redirect_url_list.clone();
@@ -108,9 +115,7 @@ pub extern "system" fn Java_cn_smilex_req_Requests__1request(
             _ => None,
         };
 
-        if let Some(r) = req {
-            let mut r = r;
-
+        if let Some(mut r) = req {
             // 设置请求参数
             if !body_status {
                 r = r.body(body);
@@ -121,47 +126,55 @@ pub extern "system" fn Java_cn_smilex_req_Requests__1request(
                 r = r.query(&v);
             }
 
-            let resp = r.send().await.unwrap();
-            status_code = resp.status();
-            version = util::version_to_str(resp.version());
+            // 发送请求
+            let resp = r.send().await;
 
-            if let Some(v) = resp.content_length() {
-                content_length = v as i64;
-            }
+            // 如果成功则设置响应数据
+            // 否则将错误信息传回body
+            if let Ok(resp) = resp {
+                status_code = resp.status();
+                version = util::version_to_str(resp.version());
 
-            if let Some(v) = resp.remote_addr() {
-                remote_address = v.to_string();
-            }
+                if let Some(v) = resp.content_length() {
+                    content_length = v as i64;
+                }
 
-            // 设置响应头
-            util::for_header_map(resp.headers(), |item| {
-                let header_name = item.0.as_str();
-                let header_value = item.1.to_str().unwrap();
+                if let Some(v) = resp.remote_addr() {
+                    remote_address = v.to_string();
+                }
 
-                let headers = env
-                    .get_field(resp_obj, "headers", util::JAVA_CLASS_IDENTITY_HASH_MAP)
-                    .unwrap()
-                    .l()
+                // 设置响应头
+                util::for_header_map(resp.headers(), |item| {
+                    let header_name = item.0.as_str();
+                    let header_value = item.1.to_str().unwrap();
+
+                    let headers = env
+                        .get_field(resp_obj, "headers", util::JAVA_CLASS_IDENTITY_HASH_MAP)
+                        .unwrap()
+                        .l()
+                        .unwrap();
+
+                    env.call_method(
+                        headers,
+                        "put",
+                        format!(
+                            "({}{}){}",
+                            util::JAVA_CLASS_OBJECT,
+                            util::JAVA_CLASS_OBJECT,
+                            util::JAVA_CLASS_OBJECT
+                        ),
+                        &[
+                            JValue::from(JObject::from(env.new_string(header_name).unwrap())),
+                            JValue::from(JObject::from(env.new_string(header_value).unwrap())),
+                        ],
+                    )
                     .unwrap();
+                });
 
-                env.call_method(
-                    headers,
-                    "put",
-                    format!(
-                        "({}{}){}",
-                        util::JAVA_CLASS_OBJECT,
-                        util::JAVA_CLASS_OBJECT,
-                        util::JAVA_CLASS_OBJECT
-                    ),
-                    &[
-                        JValue::from(JObject::from(env.new_string(header_name).unwrap())),
-                        JValue::from(JObject::from(env.new_string(header_value).unwrap())),
-                    ],
-                )
-                .unwrap();
-            });
-
-            resp_body = resp.text().await.unwrap();
+                resp_body = resp.text().await.unwrap();
+            } else if let Err(err) = resp {
+                resp_body = err.to_string();
+            }
         }
     });
 
